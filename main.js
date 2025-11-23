c5934d824b543b5948c73f7c4657b4d958d54c1a
Gskds <weddeveloper460@gmail.com>
2025-11-23 13:03:41 +0300

    update

---
+++ main.js
@@ -0,0 +1,21 @@
+const faqQuestions = document.querySelectorAll('.faq-question');
+
+faqQuestions.forEach(question => {
+    question.addEventListener('click', function() {
+        // When user clicks any question:
+        
+        // 1. Toggle current item
+        this.classList.toggle('active');
+        // 1.1 Toggle behavior for the icon
+        const answer = this.nextElementSibling;
+        answer.classList.toggle('active');
+        
+        // 2. Close siblings
+        faqQuestions.forEach(otherQuestion => {
+            if (otherQuestion !== this && otherQuestion.classList.contains('active')) {
+                otherQuestion.classList.remove('active');
+                otherQuestion.nextElementSibling.classList.remove('active');
+            }
+        });
+    });
+});
